7f197b588788f0ecf41ac977627c54424ac86752
danielpinoy <144582323+danielpinoy@users.noreply.github.com>
2023-10-30 10:21:34 +0800

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,7 +1,7 @@
 import { useState,useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-export const MainView = () => {
+ const MainView = () => {
 
 	const [movies, setMovie] = useState([]);
 	useEffect(() => {
@@ -59,3 +59,5 @@ export const MainView = () => {
     <div>{updatedMovie}</div>
   );
 }
+
+export default MainView
